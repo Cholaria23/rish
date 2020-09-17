7dcf4b956b34087a97368e31b60a3791fde7860e
valik <cardinaliv94@gmial.com>
2020-09-17 16:18:49 +0300

    17.09.2020 seo chat

--- resources/views/layouts/main/wrapper.blade.php
+++ resources/views/layouts/main/wrapper.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ App::getLocale() }}">
 	<head>
-		<base href="{{ URL::to('/') }}/" />
+		<base href="{{ URL::to('/') }}" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=0">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
