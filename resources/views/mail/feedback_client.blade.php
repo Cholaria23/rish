6a4863c13b41319f944f42301f58dc7c7e8207b2
sirenkod <sirenko_denis86@gmail.com>
2020-09-30 14:34:36 +0300

    fix emails

--- resources/views/mail/feedback_client.blade.php
+++ resources/views/mail/feedback_client.blade.php
@@ -80,7 +80,7 @@
                                                                 <p style="margin: 0; padding: 15px 0; text-align: left; font-size: 14px; color: #343434;; font-weight: bold; border-bottom: 1px solid #f2f2f2;">
                                                                     <b>@Lang('main.form.your_question'):</b>
                                                                 </p>
-                                                                <p style="display: flex; align-items: center; justify-content: space-between;font-size: 14px; margin: 0; padding: 10px 0; color: #343434; font-weight: 400; border-bottom: 1px solid #f2f2f2;">
+                                                                <p style="font-size: 14px; margin: 0; padding: 10px 0; color: #343434; font-weight: 400; border-bottom: 1px solid #f2f2f2;">
                                                                     {{ $content }}
                                                                 </p>
                                                             @endif
