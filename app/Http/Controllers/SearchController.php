bea039dccc150793df9bac660b616af85fe770e0
valik <cardinaliv94@gmial.com>
2020-09-30 10:14:48 +0300

    service search item

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -62,6 +62,12 @@ class SearchController extends Controller
             $equipments_cats = array_merge([7], Cat::descendants(7));
             $specialists_cats = array_merge([5], Cat::descendants(5));
 
+            $response['services_cats'] = $services_cats;
+            $response['news_cats'] = $news_cats;
+            $response['articles_cats'] = $articles_cats;
+            $response['actions_cats'] = $actions_cats;
+            $response['equipments_cats'] = $equipments_cats;
+            $response['specialists_cats'] = $specialists_cats;
 
             $units = Unit::with('lang')->whereHas('category', function ($query) {
                 $query->with('lang')->where('is_hidden',0);
@@ -347,6 +353,7 @@ class SearchController extends Controller
         $response = [
             'name'             => str_ireplace($search, '<b>'.$search.'</b>', $unit->lang->name),
             'cat_name'         => $unit->category->lang->name,
+            'cat_id'           => $unit->category->id,
             'short_desc_1'     => str_ireplace($search, '<b>'.$search.'</b>', mb_strtolower($unit->lang->short_desc_1)),
             'short_desc_2'     => str_ireplace($search, '<b>'.$search.'</b>', mb_strtolower($unit->lang->short_desc_2)),
             'date_publication' => ($unit->date_publication != '') ? $unit->date_publication->format('d.m.Y') : NULL,
